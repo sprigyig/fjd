38b2f11593340f844eaead55ab549869799eb9c0
Mitch <oss@mskiba.net>
2013-10-16 10:06:36 -0700

    Added row_bounds functions that allow many lines to be discovered in one go - will cut out extra linear searching for common editor refresh tasks

--- rope.py
+++ rope.py
@@ -1,3 +1,8 @@
+import sys
+def dprint(*args):
+	#sys.stdout.write(' '.join([str(x) for x in args])+'\n')
+	pass
+
 class _Node:
 	__join_min = 16
 	__imbal_tol = 5
@@ -80,6 +85,21 @@ class _Node:
 		else:
 			return s.l.index(row, col)
 
+	def row_bounds(s, first, num, offset, list, depth = 0):
+		added = 0
+		if first > s.l.nl:
+			dprint (' '*depth, 'going right only')
+			added += s.r.row_bounds(first - s.l.nl, num, offset+s.l.len, list, depth=depth+1)
+		else:
+			dprint (' '*depth,'going left')
+			added += s.l.row_bounds(first, num, offset, list, depth=depth+1)
+			num -= added
+			if num > 0:
+				dprint (' '*depth, 'going right as well')
+				added+=s.r.row_bounds(0, num, offset+s.l.len, list, depth=depth+1)
+
+		return added
+
 class _Leaf:
 	__split_min = 32
 
@@ -132,6 +152,35 @@ class _Leaf:
 
 		return i+col
 
+	def row_bounds(s, first, num, offset, list, depth=0):
+		dprint (' '*depth, first, num, offset, list, s.c.__repr__())
+		added = 0
+		if offset == 0 and first == 0:
+			dprint(' '*depth, 'adding bare 0')
+			list.append(0)
+			added+=1
+			num-=1
+
+		i = 0
+		while i<len(s.c) and first:
+			if s.c[i]=='\n':
+				first-=1
+			i+=1
+
+		if i:
+			i-=1
+
+		while i<len(s.c) and num:
+			if s.c[i]=='\n':
+				num-=1
+				added+=1
+				dprint(' '*depth, 'adding', i, offset, i+offset+1)
+				list.append(i+offset+1)
+			i+=1
+
+		return added
+				
+
 class Rope:
 	def __init__(self, init="", node=None):
 		if not node:
@@ -160,7 +209,25 @@ class Rope:
 	def insert(self, seq, index):
 		self.node = self.node.insert(seq, index)
 
+	def row_bounds(self, first, num):
+		l = []
+		self.node.row_bounds(first, num + 1, 0, l)
+		return l
+
+	def lines(self, first, num):
+		bounds = self.row_bounds(first, num)
+		dprint(bounds)
+		return [self[a:b] for a,b in zip(bounds[:-1],bounds[1:])]
+
 test = Rope(node=_Node(_Leaf("line1 \nline2\n"),_Node(_Leaf("line3"),_Leaf("\nline4"))))
 test2 = Rope()
 for i in range(100):
 	test2.insert("line %d\n"%i, len(test2))
+
+
+
+for i in range(98):
+	lines = tuple(test2.lines(i, 3))
+	if lines != tuple(['line %d\n'%(i+x) for x in range(3)]):
+		print test2.lines(i, 3), 'vs', ['line %d\n'%(i+x) for x in range(3)]
+
