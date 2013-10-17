7ec7ecbfa7d388f164036aedec327c405a99a45c
Mitch <oss@mskiba.net>
2013-10-16 20:03:43 -0700

    last throwaway change set on the old stuff... maybe

--- window.py
+++ window.py
@@ -4,6 +4,8 @@ import cairo
 bg = (1,1,1)
 fg = (0,0,0)
 
+styles = [ (bg, fg), (fg, bg)]
+
 class MyW(gtk.DrawingArea):
 	__gsignals__ = { "expose-event":"override"}
 
@@ -13,7 +15,7 @@ class MyW(gtk.DrawingArea):
 
 	def do_expose_event(self, event):
 		width, height = self.window.get_size()
-		cw,ch,cs = 8,11,11
+		cw,ch,cs = 8,12,11
 		cr = self.window.cairo_create()
 		cr.set_source_rgb(*bg)
 		cr.rectangle(0,0,width,height)
@@ -23,35 +25,71 @@ class MyW(gtk.DrawingArea):
 		cr.select_font_face("Courier")
 		cr.set_font_size(cs)
 
-		for row in range(height//ch):
+		for row in range(height//ch)[::-1]:
 			for col in range(width//cw):
-				cr.move_to(cw*col, ch * (row + 1))
-				cr.show_text(self.content(row,col))
+				x = cw*col
+				y = ch * (row +1)
+				cr.set_source_rgb(*styles[self.csrc.style(row,col)][0])
+				cr.rectangle(x,y-ch+3,cw,ch)
+				cr.fill()
+				cr.set_source_rgb(*styles[self.csrc.style(row,col)][1])
+				cr.move_to(x, y)
+				cr.show_text(self.csrc.content(row,col))
 
 
 class ThrowawayDocument:
 	def __init__(self):
-		self.lines = [[]]
+		self.lines = [""]
+		self.cursor = (0,0)
 
 	def add(self, chr):
+		row = self.cursor[0]
+		col = self.cursor[1]
 		if chr == '\n':
-			self.lines += [[]]
+			cur, nl = self.lines[row][:col],self.lines[row][col:]
+			self.lines = self.lines[:row]+[cur, nl]+self.lines[row+1:]
+			self.cursor = (row+1, 0)
 		else:
-			self.lines[-1]+=[chr]
+			self.lines[row] = self.lines[row][:col] + str(chr) + self.lines[row][col:]
+			self.cursor = (row, col+1)
+
+		print self.cursor
+			
 
 	def content(self, row, col):
 		if row < len(self.lines):
 			r = self.lines[row]
 			if col < len(r):
 				return r[col]
-			elif col == len(r) and row == len(self.lines)-1:
-				return '|'
 		return ' '
-	def back(self):
-		if self.lines[-1]:
-			self.lines[-1] = self.lines[-1][:-1]
-		elif len(self.lines) - 1:
-			self.lines = self.lines[:-1]
+
+	def style(self, row, col):
+		return 1 if row==self.cursor[0] and col==self.cursor[1] else 0
+
+	def special(self, keycode):
+		row, col = self.cursor[0], self.cursor[1]
+		if keycode == gtk.keysyms.BackSpace:
+			if col > 0:
+				self.lines[row] = self.lines[row][:col-1]+self.lines[row][col:]
+				self.cursor = (row, col-1)
+			elif row > 0:
+				print "cursor", self.cursor
+				print "lines", self.lines
+				self.cursor = (row-1, len(self.lines[row-1]))
+				print "part1", self.lines[:row-1]
+				print "part2", [(self.lines[row-1]+self.lines[row])]
+				print "part3", self.lines[row+1:]
+				self.lines = self.lines[:row-1]+ [(self.lines[row-1]+self.lines[row])] + self.lines[row+1:]
+		if keycode == gtk.keysyms.Left: 
+			if col > 0:
+				self.cursor = (row, col-1)
+			else:
+				self.cursor = (row-1, len(self.lines[row-1]))
+		elif keycode == gtk.keysyms.Right:
+			if col < len(self.lines[row]):
+				self.cursor = (row, col+1)
+			else:
+				self.cursor = (row+1, 0)
 
 t = ThrowawayDocument()
 w = MyW(t)
@@ -69,8 +107,8 @@ def kepress(w, e):
 		t.add(chr(e.keyval))
 	elif (e.keyval == gtk.keysyms.Return):
 		t.add('\n')
-	elif (e.keyval == gtk.keysyms.BackSpace):
-		t.back()
+	else:
+		t.special(e.keyval)
 	w.queue_draw()
 
 window = gtk.Window()
